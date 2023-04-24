a0d208a88bccca6fd13899643402793098a36dda
toxiczephyr08 <rashi.bindra@gmail.com>
2023-04-24 23:17:24 +0530

    Updated group id in accordance with other projects. project directory renamed

--- eureka/src/main/java/com/sweethome/eureka/EurekaServerApplication.java
+++ eureka/src/main/java/com/sweethome/eureka/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.upgrad.fsd.eureka;
+package com.sweethome.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
